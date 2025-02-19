c08c418555807a5aade92f9d9d16e41f0b9a8787
OmarAyman85 <oamr.shivy85@gmail.com>
2025-02-19 17:15:09 +0200

    domain layer --> usecases

--- lib/features/posts/domain/usecases/get_all_posts.dart
+++ lib/features/posts/domain/usecases/get_all_posts.dart
@@ -1,15 +1,15 @@
+import '../repositories/posts_repository.dart';
 import 'package:dartz/dartz.dart';
 
 import '../../../../core/errors/failures.dart';
 import '../entities/post.dart';
-import '../repositories/posts_repository.dart';
 
-class UpdatePostUsecase {
+class GetAllPostsUsecase {
   final PostsRepository repository;
 
-  UpdatePostUsecase(this.repository);
+  GetAllPostsUsecase(this.repository);
 
-  Future<Either<Failure, Unit>> call(Post post) async {
-    return await repository.updatePost(post);
+  Future<Either<Failure, List<Post>>> call() async {
+    return await repository.getAllPosts();
   }
 }

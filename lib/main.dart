53f02157a1de077c930b5fbba147b53d5ecce90e
OmarAyman85 <oamr.shivy85@gmail.com>
2025-02-21 16:43:27 +0200

    Posts page + widgets + main

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:focusflow/core/utils/app_theme.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
-  runApp(FocusFLowApp());
+import 'core/utils/app_theme.dart';
+import 'features/posts/presentation/bloc/add_delete_update_post/bloc/add_delete_update_post_bloc.dart';
+import 'features/posts/presentation/bloc/posts/posts_bloc.dart';
+import 'features/posts/presentation/pages/posts_page.dart';
+import 'injection_container.dart' as di;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
+  runApp(MyApp());
 }
 
-class FocusFLowApp extends StatelessWidget {
-  const FocusFLowApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: getLightTheme(),
-      title: "Posts App",
-      home: Scaffold(
-        appBar: AppBar(title: Text("Posts App")),
-        body: Center(child: Text("Hello World !!!")),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => di.sl<PostsBloc>()..add(GetAllPostsEvent()),
+        ),
+        BlocProvider(create: (_) => di.sl<AddDeleteUpdatePostBloc>()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: getLightTheme(),
+        title: 'Posts App',
+        home: PostsPage(),
       ),
     );
   }
